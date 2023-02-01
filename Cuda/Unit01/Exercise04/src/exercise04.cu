3a5437efeadb832c52b11de118301eac9a41a625
Colby <ncolbywt@gmail.com>
2023-02-01 11:18:21 -0600

    Completed unit 01 exercise 05 for Cuda

---
+++ Cuda/Unit01/Exercise04/src/exercise04.cu
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+// Don't want to keep writing std::
+using namespace std;
+
+// Function to square a number using the GPU
+__global__
+void square(float *n) {
+    n[0] = n[0]*n[0];
+}
+
+int main() {
+    // Setting the variable to get and manipulate
+    float *num;
+
+    // Allocating memory on the GPU
+    cudaMallocManaged(&num, sizeof(float));
+
+    // Reading in the value
+    ifstream indata;
+    indata.open("../Unit01/Exercise04/Inputs/input.txt");
+    indata >> num[0];
+
+    // Squaring the value
+    square<<<1,1>>>(num);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
+    // Writing out the result
+    ofstream outdata;
+    outdata.open("../Unit01/Exercise04/Outputs/output.txt");
+    outdata << num[0];
+
+    // Freeint memory
+    cudaFree(num);
+
+    return 0;
+}
